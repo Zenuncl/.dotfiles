61b32c97f19c51533093af19743a4198bd0f6078
David Jiang <SharkIng@Shacas.com>
2019-04-15 02:24:12 -0400

    ZSH configs init commit

---
+++ zsh/alias.plugin.zsh
@@ -0,0 +1,5 @@
+# Aliases Set Up
+
+# UTC time check
+alias utc_time="TZ='Etc/UTC' date"
+alias utc_chrome="TZ='Etc/UTC' open -na 'Google Chrome' --args '--user-data-dir=$HOME/.chrome-testing-profile'"
---
+++ zsh/skywalker.plugin.zsh
@@ -0,0 +1,30 @@
+# ZSH History ignore space
+setopt HIST_IGNORE_SPACE
+
+# GNUPG SSH Auth for login to SSH with Yubikey
+SSH_AUTH_SOCK=$HOME/.gnupg/S.gpg-agent.ssh; export SSH_AUTH_SOCK;
+
+# .nvm
+export NVM_DIR=$HOME/.nvm
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"  # This loads nvm
+
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="$PATH:$HOME/.rvm/bin"
+
+# GOPATH
+export GOPATH=$HOME/dev/go
+export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin" # Add Go lang PATH
+
+# Shortcut to projects
+export DOTFILES=$HOME/.dotfiles
+export WORKSPACE=$HOME/dev
+export PROJECT=$HOME/dev/projects
+
+# Go Version Manager
+source $HOME/.gvm/scripts/gvm
+
+# Rust env
+source $HOME/.cargo/env
+
+# Global env
+source $HOME/.env
