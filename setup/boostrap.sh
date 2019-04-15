37216c10cf3dcc294e11549df8b24c74d1834470
David Jiang <SharkIng@Shacas.com>
2019-04-15 19:00:29 -0400

    Put docker back

--- setup/boostrap.sh
+++ setup/boostrap.sh
@@ -100,3 +100,8 @@ install_ruby
 ${DOTFILES}/setup/system/plugins.sh $USERNAME
 ${DOTFILES}/setup/system/vim.sh $USERNAME
 ${DOTFILES}/setup/system/tmux.sh $USERNAME
+${DOTFILES}/setup/system/docker.sh
+
+
+# Mike Directory
+mkdir -p ${HOME}/dev/{$USER,repos,go,dockers,scripts,projects,virtualenv}
--- setup/post.sh
+++ setup/post.sh
@@ -1,8 +1,4 @@
 # Install tmuxinator (Optional, need ruby-gem)
 gem install tmuxinator
 
-# Install applications (using apt-get need sudo)
-${DOTFILES}/setup/docker/docker.sh
 
-# Mike Directory
-mkdir -p ${HOME}/dev/{$USER,repos,go,dockers,scripts,projects,virtualenv}
