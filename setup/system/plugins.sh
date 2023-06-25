a3b4f50cbfaabbc48a031accb994e6b43c304bcb
Zavier Jiang <SharkIng@Shacas.com>
2023-06-25 00:27:03 +0000

    Test bug fix

--- setup/system/plugins.sh
+++ setup/system/plugins.sh
@@ -27,7 +27,6 @@ git clone \
   https://github.com/zsh-users/zsh-autosuggestions.git \
   ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-
 # Install zsh-syntax-highlighting
 git clone \
   https://github.com/zsh-users/zsh-syntax-highlighting.git \
