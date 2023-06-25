5c7888f0e745b62ca0d109c859137eb0f9455d37
Zavier Jiang <SharkIng@Shacas.com>
2023-06-25 00:24:13 +0000

    Fix bug

--- setup/system/plugins.sh
+++ setup/system/plugins.sh
@@ -31,7 +31,6 @@ git clone \
 # Install zsh-syntax-highlighting
 git clone \
   https://github.com/zsh-users/zsh-syntax-highlighting.git \
-
   ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo "Done"
