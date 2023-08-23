7d25d0f3c5f41878f335f4326df6db7f713ca109
Anonymous <anonymous@risea.net>
2023-08-23 02:33:40 -0400

    Convert zsh-plugin script to /bin/sh capatible

--- setup/system/plugins.sh
+++ setup/system/plugins.sh
@@ -7,29 +7,29 @@ git clone \
 
 cd /tmp/autojump/ && \
   ./install.py && \
-  cd -
+  cd - || exit
 
 # Install fzf
 git clone \
   --depth 1 \
   https://github.com/junegunn/fzf.git \
-  $HOME/.fzf
+  "$HOME"/.fzf
 
-$HOME/.fzf/install
+"$HOME"/.fzf/install
 
 # Install auto-completions
 git clone \
   https://github.com/zsh-users/zsh-completions.git \
-  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-completions
+  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-completions
 
 # Install autosuggestion
 git clone \
   https://github.com/zsh-users/zsh-autosuggestions.git \
-  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-autosuggestions
 
 # Install zsh-syntax-highlighting
 git clone \
   https://github.com/zsh-users/zsh-syntax-highlighting.git \
-  ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+  "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}"/plugins/zsh-syntax-highlighting
 
 echo "Done"
