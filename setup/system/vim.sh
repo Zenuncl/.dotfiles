099f0300c0c799299f4c6c712e9a8f8f8675fa64
Anonymous <anonymous@risea.net>
2023-08-23 02:25:36 -0400

    Convert vim script to /bin/sh capatible

--- setup/system/vim.sh
+++ setup/system/vim.sh
@@ -1,21 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
 # Remove .vim folder
-rm -rf $HOME/.vim
+rm -rf "$HOME"/.vim
 
 # Install .vim dotfile
 git clone \
   https://github.com/gpakosz/.vim.git \
-  $HOME/.vim
+  "$HOME"/.vim
 
 # Setup .vim
-ln -fs $HOME/.vim/.vimrc $HOME
+ln -fs "$HOME"/.vim/.vimrc "$HOME"
 
 # Symlink custom file
-ln -fs $HOME/.dotfiles/vim/vimrc.local.symlink $HOME/.vimrc.local # use dotfiles local
+ln -fs "$HOME"/.dotfiles/vim/vimrc.local.symlink "$HOME"/.vimrc.local # use dotfiles local
 
 # Use heavenly branch with add-ons
-cd $HOME/.vim && \
+cd "$HOME"/.vim && \
   git checkout heavenly && \
   git submodule init && \
   git submodule update
