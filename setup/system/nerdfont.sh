83fd96a010cdbd19f3c4419980791ee669435e49
Anonymous <anonymous@risea.net>
2023-08-23 02:23:01 -0400

    Make NerdFont script /bin/sh compatible

--- setup/system/nerdfont.sh
+++ setup/system/nerdfont.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 DIRECTORY=$HOME/.local/nerdfont
 DEFAULT_FONT="SourceCodePro"
@@ -6,13 +6,16 @@ DEFAULT_FONT="SourceCodePro"
 # Check if DIRECTORY exist
 if [ ! -d "$DIRECTORY" ]; then
   echo "$DIRECTORY does not exist... Creating..."
-  mkdir -p ${DIRECTORY}
+  mkdir -p "${DIRECTORY}"
 fi
 
 # Clone nerdfont repo
-git clone \
+git clone --filter=blob:none --sparse \
   https://github.com/ryanoasis/nerd-fonts.git \
-  $DIRECTORY
+  "$DIRECTORY"
 
 # Install Font
-$DIRECTORY/install.sh $DEFAULT_FONT
+cd "$DIRECTORY" && \
+  git sparse-checkout add patched-fonts/$DEFAULT_FONT
+
+"$DIRECTORY"/install.sh "$DEFAULT_FONT"
