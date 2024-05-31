9c50f9e5533e0d5ecf4f4bf06c66f5c9244b2a1a
Zavier Jiang <Zavi@Zenuncl.com>
2024-05-31 07:34:25 +0000

    Update install packages script for hyprland

--- setup/hyprland/setup.sh
+++ setup/hyprland/setup.sh
@@ -1,1 +1,39 @@
-sudo pacman -S hyprland xorg-xwayland waybar mako blueman networkmanager fcitx5 btop yazi thunar flameshot grim imv neovim mpv kitty copyq --needed
+packagesPacman=(
+  "hyprland"
+  "hyprpaper"
+  "hyprlock"
+  "hypridle"
+  "xdg-desktop-portal-hyprland"
+  "xorg-xwayland"
+  "waybar"
+  "mako"
+  "grim"
+  "slurp"
+  "swappy"
+  "cliphist"
+  "blueman"
+  "networkmanager"
+  "fcitx5"
+  "btop"
+  "yazi"
+  "thunar"
+  "flameshot"
+  "imv"
+  "neovim"
+  "mpv"
+  "kitty"
+  "copyq"
+  )
+
+packagesYay=(
+  "wlogout"
+  "eww"
+  "lightdm-settings"
+  "anyrun-git"
+  )
+
+
+
+pacman --noconfirm --needed -S ${packagesPacman}
+
+yay --noconfirm --needed -S ${packagesYay}
--- setup/hyprland/yay.sh
+++
@@ -1,1 +0,0 @@
-yay -S lightdm-settings anyrun-git --needed
