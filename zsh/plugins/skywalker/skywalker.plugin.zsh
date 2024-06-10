69a2131eb956b5dc4d009ac77bc78618fa8d9781
Zavier Jiang <Zavi@Zenuncl.com>
2024-06-10 04:10:32 +0000

    Add envman to ohmyzsh plugin

--- zsh/plugins/skywalker/skywalker.plugin.zsh
+++ zsh/plugins/skywalker/skywalker.plugin.zsh
@@ -62,4 +62,7 @@ fi
 
 [ -s "$HOME/.rsvm/rsvm.sh" ] && source "$HOME/.rsvm/rsvm.sh" # This loads RSVM
 
+# Generated for envman. Do not edit.
+[ -s "$HOME/.config/envman/load.sh" ] && source "$HOME/.config/envman/load.sh"
+
 autoload -U compinit && compinit -u
