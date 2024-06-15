8cb6b249270e0a7215fda3591797334c184d3266
Zavier Jiang <Zavi@Zenuncl.com>
2024-06-15 01:25:08 -0400

    Change OMF Fish configs

--- config/omf/custom/alias.fish
+++ config/omf/custom/alias.fish
@@ -1,2 +1,84 @@
-# Alias
+# Aliases Set Up
+
+# Fish
 alias fishp="fish --private"
+
+# UTC time check
+alias utc_time="TZ='Etc/UTC' date"
+alias nyc_time="TZ='Canada/Eastern' date"
+alias sha_time="TZ='Asia/Shanghai' date"
+alias par_time="TZ='Europe/Paris' date"
+alias tyo_time="TZ='Asia/Tokyo' date"
+alias utc_chrome="TZ='Etc/UTC' open -na 'Google Chrome' --args '--user-data-dir=$HOME/.chrome-testing-profile'"
+
+# System level alias
+alias reload!=". $HOME/.zshrc"
+alias less="less -R" # -R preserves ANSI color escape sequences in output
+alias grep="grep --color=auto"
+alias ff="find . -type f -name" # Fast find
+alias du="du -h"
+alias df="df -h"
+alias javac="javac -J-Dfile.encoding=utf8"
+alias ls='ls --color=auto'
+alias free="free -h"
+alias top="htop"
+alias ps="ps auxf"
+#alias vi="vim"
+alias vi="nvim"
+alias nvi="nvim"
+alias mutt="neomutt"
+alias c="clear"
+alias x="exit"
+alias h="history -20"
+alias hc="history -c"
+alias hg="history | grep"
+alias ag="alias | grep"
+alias t="htop || top"
+alias which="type -a"
+alias curl="curlie"
+alias zj="start-zellij"
+
+
+# Change directory
+alias pud="pushd"
+alias ppd="popd"
+
+# Other useful alias
+alias latex="docker run --rm --volume "$(pwd):/data" --user $(id -u):$(id -g) pandoc/latex"
+
+# Global alias
+alias -g G="| grep"
+alias -g L="| less"
+alias -g C="| column -t"
+
+# File alias
+alias -s gz='tar -xzvf'
+alias -s tgz='tar -xzvf'
+alias -s zip='unzip'
+alias -s bz2='tar -xjvf'
+
+# Default File opener alias
+alias -s html=vim
+alias -s rb=vim
+alias -s py=vim
+alias -s js=vim
+alias -s css=vim
+alias -s c=vim
+alias -s java=vim
+alias -s php=vim
+alias -s txt=vim
+
+# User configuration
+alias last.command='fc -ln -1'
+alias ll="ls -lh"
+alias la="ls -lha"
+alias cp="cp -i"
+alias mv="mv -i"
+alias rm="rm -i"
+alias mkdir="mkdir -p"
+alias ..="cd .."
+alias ...="cd ../../"
+alias vi="vim"
+alias weather="curl wttr.in"
+alias ipinfo="curl ipinfo.io"
+alias ffs-"sudo !!"
--- config/omf/init.fish
+++ config/omf/init.fish
@@ -2,15 +2,15 @@
 set --global fish_greeting
 
 # Environment variable
-set --global --export GOPATH $HOME/dev/golang
-set --global --export GOROOT $HOME/dev/golang
-set --global --export EDITOR /usr/bin/nvim
+set --global --export EDITOR  /usr/bin/nvim
 
-set --global --export PATH  $PATH \
-                            $HOME/.bin \
-                            $GOPATH/bin \
-                            $GOROOT/bin
+set --global --export PATH    $PATH \
+                              $HOME/.bin \
+                              $GOPATH/bin
 
 # Load Custom files
 source $HOME/.config/omf/custom/alias.fish
 source $HOME/.config/omf/custom/function.fish
+
+
+ssh-add $HOME/.ssh/secrets/keys.sec/github.ed25519.pem
