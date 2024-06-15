cf1761b29b7544460371e047de9d155a6986a150
Zavier Jiang <Zavi@Zenuncl.com>
2024-06-15 02:11:01 -0400

    OMF config

--- config/omf/custom/alias.fish
+++ config/omf/custom/alias.fish
@@ -9,35 +9,27 @@ alias nyc_time="TZ='Canada/Eastern' date"
 alias sha_time="TZ='Asia/Shanghai' date"
 alias par_time="TZ='Europe/Paris' date"
 alias tyo_time="TZ='Asia/Tokyo' date"
-alias utc_chrome="TZ='Etc/UTC' open -na 'Google Chrome' --args '--user-data-dir=$HOME/.chrome-testing-profile'"
 
 # System level alias
-alias reload!=". $HOME/.zshrc"
-alias less="less -R" # -R preserves ANSI color escape sequences in output
+alias less="less --raw-control-chars" # -R preserves ANSI color escape sequences in output
 alias grep="grep --color=auto"
 alias ff="find . -type f -name" # Fast find
-alias du="du -h"
-alias df="df -h"
+alias du="du --human-readable"
+alias df="df --human-readable"
 alias javac="javac -J-Dfile.encoding=utf8"
-alias ls='ls --color=auto'
-alias free="free -h"
-alias top="htop"
+alias ls="ls -all --human-readable --color=auto"
+alias ll="ls -l --human-readable --color=auto"
+alias la="ls -l --all --human-readable --color=auto"
+alias free="free --human-readable"
 alias ps="ps auxf"
-#alias vi="vim"
-alias vi="nvim"
-alias nvi="nvim"
-alias mutt="neomutt"
 alias c="clear"
 alias x="exit"
 alias h="history -20"
-alias hc="history -c"
+alias hc="history --contains"
 alias hg="history | grep"
 alias ag="alias | grep"
 alias t="htop || top"
-alias which="type -a"
-alias curl="curlie"
-alias zj="start-zellij"
-
+alias which="type --all"
 
 # Change directory
 alias pud="pushd"
@@ -46,39 +38,24 @@ alias ppd="popd"
 # Other useful alias
 alias latex="docker run --rm --volume "$(pwd):/data" --user $(id -u):$(id -g) pandoc/latex"
 
-# Global alias
-alias -g G="| grep"
-alias -g L="| less"
-alias -g C="| column -t"
-
 # File alias
-alias -s gz='tar -xzvf'
-alias -s tgz='tar -xzvf'
-alias -s zip='unzip'
-alias -s bz2='tar -xjvf'
-
-# Default File opener alias
-alias -s html=vim
-alias -s rb=vim
-alias -s py=vim
-alias -s js=vim
-alias -s css=vim
-alias -s c=vim
-alias -s java=vim
-alias -s php=vim
-alias -s txt=vim
+alias gz='tar -xzvf'
+alias tgz='tar -xzvf'
+alias zip='unzip'
+alias bz2='tar -xjvf'
 
 # User configuration
-alias last.command='fc -ln -1'
-alias ll="ls -lh"
-alias la="ls -lha"
-alias cp="cp -i"
-alias mv="mv -i"
-alias rm="rm -i"
-alias mkdir="mkdir -p"
-alias ..="cd .."
-alias ...="cd ../../"
-alias vi="vim"
+alias top="htop"
+alias cat='bat --theme=ansi'
+alias cp="cp --interactive"
+alias mv="mv --interactive"
+alias rm="rm --interactive"
+alias mkdir="mkdir --parents"
 alias weather="curl wttr.in"
 alias ipinfo="curl ipinfo.io"
-alias ffs-"sudo !!"
+alias vi="nvim"
+alias vim="nvim"
+alias nvi="nvim"
+alias mutt="neomutt"
+alias curl="curlie || curl"
+alias zj="start-zellij"
