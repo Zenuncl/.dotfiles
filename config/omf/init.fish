8c038e811e99fe196cdc41ccffece44c07ddf70c
Zavier Jiang <Zavi@Zenuncl.com>
2024-11-26 23:03:36 -0500

    Update fish omf init

--- config/omf/init.fish
+++ config/omf/init.fish
@@ -4,15 +4,20 @@ set --global fish_greeting
 # Environment variable
 set --global --export EDITOR  /usr/bin/nvim
 
-set --global --export PATH    $PATH \
-                              $HOME/.bin \
-                              $GOPATH/bin
+set --global --export PATH    $HOME/.bin \
+                              $GOPATH/bin \
+                              $PATH
 
 # Load Custom files
+source $HOME/.config/omf/custom/env.fish
 source $HOME/.config/omf/custom/alias.fish
 source $HOME/.config/omf/custom/function.fish
 
 # Load Starship plugin
 source $HOME/.config/omf/custom/starship.fish
+
+# Load curlie plugin
+source $HOME/.config/envman/load.fish
+source $HOME/.config/omf/custom/mise.fish
 
 ssh-add $HOME/.ssh/secrets/keys.sec/github.ed25519.pem
