53b7e3d52a5bbf7f6a728605345c04b06163ca70
Zavier Jiang <Zavi@Zenuncl.com>
2025-06-18 22:40:06 -0400

    Add dotenv function

--- config/omf/custom/function.fish
+++ config/omf/custom/function.fish
@@ -1,1 +1,15 @@
 # Test
+function dotenv --description "Load .env into current fish session"
+    set -l file (test -n "$argv[1]"; and echo $argv[1]; or echo ".env")
+    if not test -f $file
+        echo "dotenv ❌  $file not found" >&2
+        return 1
+    end
+
+    for line in (string match -v '^\s*#' (cat $file | string trim -l -r))
+        set -l kv (string split -m1 '=' $line)
+        if test (count $kv) -eq 2
+            set -gx $kv[1] $kv[2]
+        end
+    end
+end
