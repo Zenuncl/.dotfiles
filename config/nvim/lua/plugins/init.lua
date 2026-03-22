2b82176f7e02641a34df34cd3db6eb4eb8e86084
Zavier Jiang <Zavi@Zenuncl.com>
2026-03-22 17:34:36 +0000

    FEAT: Add Render markdown plugin

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -6,6 +6,31 @@ return {
       require "configs.conform"
     end,
   },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },            -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    ft = { "markdown" },
+    keys = {
+      {
+        "<leader>rmd",
+        "<cmd>RenderMarkdown toggle<cr>",
+        desc = "Toggle Render Markdown",
+      },
+    },
+    config = function()
+      require('render-markdown').setup({
+        enabled = false,
+        latex = { enabled = false },
+        html = { enabled = false },
+        yaml = { enabled = false },
+      })
+    end,
+    -- opts = {},
+  },
 
   -- These are some examples, uncomment them if you want to see them work!
   -- {
