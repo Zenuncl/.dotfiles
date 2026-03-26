940adb644486f7ab7de885c3987df58fe87c7f94
Zavier Jiang <Zavi@Zenuncl.com>
2026-03-26 19:40:25 +0000

    Add auto attach tmux session when ssh

--- config/omf/custom/ssh.fish
+++ config/omf/custom/ssh.fish
@@ -2,3 +2,11 @@ if test -d $HOME/.ssh/secrets/keys.sec
   echo (set_color green)"✓ Secrets SSH Exist, Loaded..."(set_color normal)
   ssh-add $HOME/.ssh/secrets/keys.sec/github.ed25519.pem
 end
+
+if status is-interactive
+    and test "$USER" = "zenuncl"
+    and test "$hostname" = "zeus-dev.ams.atlas.ethe.net"
+    and test -z "$TMUX"
+    and test -z "$SKIP_TMUX"
+    tmux new-session -A -t Workspaces
+end
